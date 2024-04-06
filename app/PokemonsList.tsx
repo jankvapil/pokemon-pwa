3435fea80394480c0049ff33d7e8e54200bcee69
Jan Kvapil <kvapil@acrios.com>
2024-04-06 12:53:46 +0200

    Add pokemon detail page

--- app/PokemonsList.tsx
+++ app/PokemonsList.tsx
@@ -4,10 +4,16 @@ import { useGraphQLClient } from '@/components/hooks/useGraphQLClient'
 import { Dispatch, SetStateAction, useEffect, useState } from 'react'
 import { GraphQLTypes } from '@/lib/graphql/zeus'
 
-import { NonEmptyString1000, useEvolu } from '@evolu/react'
+import {
+  ExtractRow,
+  NonEmptyString1000,
+  NotNull,
+  cast,
+  useEvolu,
+  useQuery,
+} from '@evolu/react'
 import { type Database } from '@/components/providers/evoluProvider'
-
-import { FavoritesList } from './FavoritesList'
+import Link from 'next/link'
 
 type IPokemonsList = {
   showFavorites: boolean
@@ -22,8 +28,28 @@ type Pokemon = Pick<GraphQLTypes['Pokemon'], 'name' | 'id'>
 export const PokemonsList = (props: IPokemonsList) => {
   const { query } = useGraphQLClient()
   const { create } = useEvolu<Database>()
+  const { createQuery, update } = useEvolu<Database>()
 
+  const [offset, setOffset] = useState(0)
   const [pokemons, setPokemons] = useState<Array<Pokemon>>([])
+  const favorites = createQuery(
+    (db) =>
+      db
+        .selectFrom('pokemon')
+        .select(['id', 'name'])
+        .where('isDeleted', 'is not', cast(true))
+        // Filter null value and ensure non-null type.
+        .where('name', 'is not', null)
+        .$narrowType<{ name: NotNull }>()
+        .orderBy('createdAt'),
+    {
+      // logQueryExecutionTime: true,
+      // logExplainQueryPlan: true,
+    }
+  )
+
+  type PokemonRow = ExtractRow<typeof favorites>
+  const { rows } = useQuery(favorites)
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -31,7 +57,7 @@ export const PokemonsList = (props: IPokemonsList) => {
         pokemons: [
           {
             query: {
-              offset: 2,
+              offset: offset,
             },
           },
           {
@@ -42,13 +68,17 @@ export const PokemonsList = (props: IPokemonsList) => {
           },
         ],
       }).then((res) => {
-        // console.log(res)
-        setPokemons(res.pokemons.edges)
+        setPokemons((prev) => {
+          const filtered = res.pokemons.edges.filter(
+            (p) => !prev.map((p2) => p2.id).includes(p.id)
+          )
+          return [...prev, ...filtered]
+        })
       })
     }
 
     fetchPokemons()
-  }, [])
+  }, [rows, offset])
 
   /**
    * Saves pokemon to db
@@ -58,24 +88,59 @@ export const PokemonsList = (props: IPokemonsList) => {
     props.setShowFavorites(true)
   }
 
+  /**
+   * Deletes pokemon from db
+   */
+  const handleDeleteClick = (row: PokemonRow): void => {
+    update('pokemon', { id: row.id, isDeleted: true })
+  }
+
   return (
     <>
       {props.showFavorites ? (
-        <FavoritesList />
-      ) : (
-        <ul>
-          {pokemons.map((p) => (
-            <li className="border-b px-4 py-3 flex items-center" key={p.id}>
-              <span className="flex-1">{p.name}</span>
+        <ul className="py-2">
+          {rows.map((row) => (
+            <li
+              key={row.id}
+              className="border-b py-3 flex justify-center items-center gap-3"
+            >
+              <span className="text-lg font-bold">{row.name}</span>
               <button
-                className="border border-gray-200 rounded-full w-10 text-xl"
-                onClick={() => makeFavorite(p)}
+                className="_btn py-2"
+                onClick={() => handleDeleteClick(row)}
               >
-                &hearts;
+                Delete
               </button>
             </li>
           ))}
         </ul>
+      ) : (
+        <section className="flex flex-col gap-4">
+          <ul>
+            {pokemons?.map((p) => (
+              <li className="border-b px-4 py-3 flex items-center" key={p.id}>
+                <span className="flex-1">
+                  <Link href={`/${p.id}`}>
+                    {p.name}
+                    {` (${p.id})`}
+                  </Link>
+                </span>
+                <button
+                  className="border border-gray-200 rounded-full w-10 text-xl"
+                  onClick={() => makeFavorite(p)}
+                >
+                  &hearts;
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button
+            className="_btn py-2"
+            onClick={() => setOffset((prev) => prev + 10)}
+          >
+            Load more
+          </button>
+        </section>
       )}
     </>
   )
---
+++ app/[id]/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useGraphQLClient } from '@/components/hooks/useGraphQLClient'
+import { GraphQLTypes } from '@/lib/graphql/zeus'
+import { useEffect, useState } from 'react'
+
+type Pokemon = Pick<GraphQLTypes['Pokemon'], 'name' | 'image' | 'types'>
+
+/**
+ * Pokemon detail page
+ */
+export default function Page(props: any) {
+  const { query } = useGraphQLClient()
+  const [pokemon, setPokemon] = useState<Pokemon | undefined>(undefined)
+
+  useEffect(() => {
+    const fetchDetail = async () => {
+      query({
+        pokemonById: [
+          {
+            id: String(props.params.id),
+          },
+          {
+            name: true,
+            image: true,
+            types: true,
+            maxHP: true,
+            maxCP: true,
+          },
+        ],
+      }).then((res) => {
+        setPokemon(res.pokemonById)
+      })
+    }
+
+    fetchDetail()
+  }, [])
+
+  return (
+    <div>
+      <header className="bg-gray-200 flex flex-col text-center">
+        <img
+          src={pokemon?.image}
+          alt="Pokemon image"
+          className="rounded-full m-6 mb-0 w-64 h-64 self-center"
+        />
+        <h1 className="_h1 py-2">{pokemon?.name}</h1>
+        <ul className="flex gap-2 justify-center">
+          {pokemon?.types.map((type) => (
+            <li key={type}>{type}</li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  )
+}
